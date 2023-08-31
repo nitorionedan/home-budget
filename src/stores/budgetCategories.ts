e583d7ee9331775155a275dfd14dac004e5e3c53
nitorionedan <fearinatunitori@gmail.com>
2023-08-31 15:32:11 +0900

    dev: Budget category store

--- src/stores/budgetCategories.ts
+++ src/stores/budgetCategories.ts
@@ -1,26 +1,67 @@
 import { defineStore } from 'pinia';
 
 export type BudgetCategory = {
-    name: string,
-    userId: string
+  name: string
 };
 
 type State = {
   categories: Map<string, BudgetCategory>
 };
 
-export const useBudgetCategoriesStore = defineStore({
-  id: "budgetCategories",
+// Budget Category Store
+export const useBudgetCategoriesStore = defineStore('budgetCategories', {
   state: (): State => {
     return {
       categories: new Map<string, BudgetCategory>()
     }
   },
   getters: {
+    readCategories(): Map<string, BudgetCategory> {
+      return this.categories;
+    }
   },
   actions: {
+    // カテゴリー新規作成
     createCategory(budgetCategory: BudgetCategory): void {
       this.categories.set(`${this.categories.size}`, budgetCategory);
-    }
+    },
+
+    // カテゴリー情報を更新
+    updateCategory(id: string, name: string): boolean {      
+      // 存在しないIDが与えられていれば存在しないことを伝え、何もしない。
+      const notFound = !this.categories.has(id);
+      if (notFound) {
+        console.log(`id(${id})は存在しません。`);
+        return false;
+      }
+
+      // カテゴリーの情報を更新
+      const newCategory: BudgetCategory = {name: name};
+      const oldName: string | undefined = this.categories.get(id)?.name;
+      this.categories.set(id, newCategory);
+      console.log(`id(${id})のname(${oldName})を${newCategory.name}へ変更しました。`);
+      return true;
+    },
+
+    // 1件、カテゴリーデータを削除
+    deleteCategory(id: string): boolean {
+      // 存在しないIDが与えられていれば存在しないことを伝え、何もしない。
+      const notFound = !this.categories.has(id);
+      if (notFound) {
+        console.log(`id(${id})は存在しません。`);
+        return false;
+      }
+
+      this.categories.delete(id);
+      console.log(`id(${id})のカテゴリーを削除しました。`);
+      return true;
+    },
+
+    // 全件、カテゴリーデータを削除
+    deleteAllCategories(): boolean {
+      this.categories.clear();
+      console.log('すべてのカテゴリーデータを削除しました。');
+      return true;
+    },
   }
 });
--- src/views/SettingView.vue
+++ src/views/SettingView.vue
@@ -1,6 +1,46 @@
 <script setup lang="ts">
+import { useBudgetCategoriesStore, type BudgetCategory } from '@/stores/budgetCategories';
+import { ref } from 'vue';
+
+const budgetCategoriesStore = useBudgetCategoriesStore();
+const testText = ref('no unko');
+const testDeleteTargetText = ref('');
+
+function OnClickTestAddCategoryButton(): void {
+  const budgetTmp: BudgetCategory = { name: testText.value};
+  budgetCategoriesStore.createCategory(budgetTmp);
+}
+
+function OnClickTestDeleteCategoryButton(): void {
+    budgetCategoriesStore.deleteCategory(testDeleteTargetText.value);
+}
 </script>
 
 <template>
-    <h1>SettingView</h1>
+  <h1>SettingView</h1>
+  
+  <p>{{ testText }}</p>
+  <input type="text" v-model="testText"/>
+  <button @click="OnClickTestAddCategoryButton">Adding Test</button>
+
+  <p>test delete target: {{ testDeleteTargetText }}</p>
+  <input type="text" v-model="testDeleteTargetText"/>
+  <button @click="OnClickTestDeleteCategoryButton">Delete Test</button>
+
+  <table class="table">
+    <thead>
+        <tr>
+          <th>Name</th>
+          <th>UNKO1</th>
+          <th>UNKO2</th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr v-for="([key, category], index) in budgetCategoriesStore.readCategories" :key="index">
+        <td>{{ category.name }}</td>
+        <td>unko1</td>
+        <td>unko2</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
